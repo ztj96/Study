e1625dad0d5fc765a16992b1bf151203ccf3326b
Jasontjzhu <jasontjzhu@tencent.com>
2019-06-12 15:17:26 +0800

    生成项目工程

--- gn/example/build.cmd
+++ gn/example/build.cmd
@@ -1,4 +1,4 @@
 
-".\\gn\\bin\\gn.exe" gen "build_cache" --args="is_debug=false target_cpu=\"x86\" win_vc=\"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\" win_vc_ver=\"2015\" win_xp=false"
+".\\gn\\bin\\gn.exe" gen "build_cache" --ide=vs2015 --args="is_debug=false target_cpu=\"x86\" win_vc=\"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\" win_vc_ver=\"2015\" win_xp=false"
 
 ".\\gn\\bin\\ninja.exe" -C  "build_cache"
